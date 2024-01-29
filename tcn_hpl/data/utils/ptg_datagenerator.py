051332a2f0eb6cb2c1bcefec7e3ec5bd590075fe
Peri Akiva <peri.akiva@gyges.khq.kitware.com>
2024-01-29 07:03:33 -0500

    ptg datagenerator with poses and offset vectors

--- tcn_hpl/data/utils/ptg_datagenerator.py
+++ tcn_hpl/data/utils/ptg_datagenerator.py
@@ -25,17 +25,18 @@ from angel_system.data.data_paths import grab_data, data_dir
 #####################
 # Inputs
 #####################
-task = "m2_tqt"
+task = "m2"
 obj_exp_name = "p_bbn_model_m2_m3_m5_r18_v10" #"coffee+tea_yolov7"
 
 
 #  f"{ptg_root}/config/activity_labels/medical"
 # obj_dets_dir = f"{data_dir}/annotations/{recipe}/results/{obj_exp_name}"
 # obj_dets_dir = "/data/PTG/cooking/object_anns/old_coffee/results/coffee_base/" #"/home/local/KHQ/hannah.defazio/yolov7/runs/detect/coffee+tea_yolov7/"
-obj_dets_dir = "/home/local/KHQ/peri.akiva/projects/medical-pose/bbox_detection_results/RESULTS_m2_with_lab_cleaned_fixed_data_with_steps_results_train_activity_with_patient_dets.mscoco.json"
+# obj_dets_dir = "/home/local/KHQ/peri.akiva/projects/medical-pose/bbox_detection_results/RESULTS_m2_with_lab_cleaned_fixed_data_with_steps_results_train_activity_with_patient_dets.mscoco.json"
 ptg_root = "/home/local/KHQ/peri.akiva/angel_system"
 activity_config_path = f"{ptg_root}/config/activity_labels/medical"
-activity_config_fn = f"{activity_config_path}/task_{task}.yaml"
+# activity_config_fn = f"{activity_config_path}/task_{task}.yaml"
+activity_config_fn = "/home/local/KHQ/peri.akiva/projects/angel_system/config/activity_labels/medical_tourniquet.v3.yaml"
 
 feat_version = 6
 using_done = False # Set the gt according to when an activity is done
@@ -43,7 +44,7 @@ using_done = False # Set the gt according to when an activity is done
 #####################
 # Output
 #####################
-exp_name = "m2_tqt_data_test_feat_v6"#f"coffee_and_tea_feat_v{str(feat_version)}"
+exp_name = "p_m2_tqt_data_test_feat_v6"#f"coffee_and_tea_feat_v{str(feat_version)}"
 if using_done:
     exp_name = f"{exp_name}_done_gt"
 
@@ -84,12 +85,21 @@ with open(f"{output_data_dir}/mapping.txt", "w") as mapping:
 # bundles
 #####################
 for split in ["train_activity", "val", "test"]:
-    kwcoco_file = f"{obj_dets_dir}/{obj_exp_name}_results_{split}_conf_0.1_plus_hl_hands_new_obj_labels.mscoco.json"
+    # kwcoco_file = f"{obj_dets_dir}/{obj_exp_name}_results_{split}_conf_0.1_plus_hl_hands_new_obj_labels.mscoco.json"
+    kwcoco_file = "/home/local/KHQ/peri.akiva/projects/medical-pose/ViTPose/results/RESULTS_m2_with_lab_cleaned_fixed_data_with_steps_results_train_activity_with_patient_dets_with_pose.mscoco.json"
     dset = kwcoco.CocoDataset(kwcoco_file)
 
+    # print(f"kps cats: {dset.keypoint_categories()}")
+    
     for video_id in ub.ProgIter(
         dset.index.videos.keys(), desc=f"Creating features for videos in {split}"
     ):
+        
+        # if video_id != 10:
+        #     continue
+        
+        
+        
         video = dset.index.videos[video_id]
         video_name = video["name"]
         if "_extracted" in video_name:
@@ -97,6 +107,8 @@ for split in ["train_activity", "val", "test"]:
 
         image_ids = dset.index.vidid_to_gids[video_id]
         num_images = len(image_ids)
+        
+        print(f"there are {num_images} in video {video_id}")
 
         video_dset = dset.subset(gids=image_ids, copy=True)
 
