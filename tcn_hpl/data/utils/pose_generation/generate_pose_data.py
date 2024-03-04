9ec2ff2ae1eb31773fe696cfd1543eaf3963033c
Peri Akiva <peri.akiva@rutgers.edu>
2024-03-04 09:30:58 -0500

    datagenmerator for all tasks, pose data generation for all tasks

--- tcn_hpl/data/utils/pose_generation/generate_pose_data.py
+++ tcn_hpl/data/utils/pose_generation/generate_pose_data.py
@@ -52,9 +52,11 @@ class PosesGenerator(object):
         self.root_path = config['root']
         # self.paths = utils.dictionary_contents(config['root'], types=['*.JPG', '*.jpg', '*.JPEG', '*.jpeg', '*.png'], recursive=True)
         
-        self.train_dataset = kwcoco.CocoDataset(config['data']['train'])
-        self.val_dataset = kwcoco.CocoDataset(config['data']['val'])
-        self.test_dataset = kwcoco.CocoDataset(config['data']['test'])
+        # self.train_dataset = kwcoco.CocoDataset(config['data']['train'])
+        # self.val_dataset = kwcoco.CocoDataset(config['data']['val'])
+        # self.test_dataset = kwcoco.CocoDataset(config['data']['test'])
+        
+        self.dataset = kwcoco.CocoDataset(config['data'][config['task']])
         
         self.keypoints_cats = [
                         "nose", "mouth", "throat","chest","stomach","left_upper_arm",
@@ -65,10 +67,13 @@ class PosesGenerator(object):
                     ]
     
         self.keypoints_cats_dset = [{'name': value, 'id': index} for index, value in enumerate(self.keypoints_cats)]
-        self.train_dataset.dataset['keypoint_categories'] = self.keypoints_cats_dset
-        self.val_dataset.dataset['keypoint_categories'] = self.keypoints_cats_dset
-        self.test_dataset.dataset['keypoint_categories'] = self.keypoints_cats_dset
+        # self.train_dataset.dataset['keypoint_categories'] = self.keypoints_cats_dset
+        # self.val_dataset.dataset['keypoint_categories'] = self.keypoints_cats_dset
+        # self.test_dataset.dataset['keypoint_categories'] = self.keypoints_cats_dset
         
+        self.dataset.dataset['keypoint_categories'] = self.keypoints_cats_dset
+        
+        self.dataset_path_name = self.config['data'][self.config['task']][:-12].split('/')[-1] #remove .mscoco.json
         
         self.args = utils.get_parser(self.config['detection_model_config']).parse_args()
         detecron_cfg = setup_detectron_cfg(self.args)
@@ -88,9 +93,10 @@ class PosesGenerator(object):
         else:
             self.pose_dataset_info = DatasetInfo(self.pose_dataset_info)
         
-    def generate_bbs_and_pose(self, dset):
+    def generate_bbs_and_pose(self, dset, save_intermediate=True):
         
-        # json_file = utils.initialize_coco_json()
+        # json_file = utils.initialize_coco_json()ds
+        dsets_paths = []
         patient_cid = dset.add_category('patient')
         user_cid = dset.add_category('user')
         # pbar = tqdm.tqdm(enumerate(self.paths), total=len(self.paths))
@@ -154,16 +160,22 @@ class PosesGenerator(object):
                     pose_keypoints = pose_results[0]['keypoints'].tolist()
                     # bbox = pose_results[0]['bbox'].tolist()
                     pose_keypoints_list = []
-                    for index, keypoint in enumerate(pose_keypoints):
+                    for kp_index, keypoint in enumerate(pose_keypoints):
                         kp_dict = {'xy': [keypoint[0], keypoint[1]], 
-                                'keypoint_category_id': index, 
-                                'keypoint_category': self.keypoints_cats[index]}
+                                'keypoint_category_id': kp_index, 
+                                'keypoint_category': self.keypoints_cats[kp_index]}
                         pose_keypoints_list.append(kp_dict)
                     
                     current_ann['keypoints'] = pose_keypoints_list
-                    current_ann['image_features'] = image_features
+                    # current_ann['image_features'] = image_features
                     
                     dset.add_annotation(**current_ann)
+            
+            if save_intermediate:
+                if (index % 45000) == 0:
+                    dset_inter_name = f"{self.config['data']['save_root']}/{self.dataset_path_name}_{index}_with_dets_and_pose.mscoco.json"
+                    dset.dump(dset_inter_name, newlines=True)
+                    print(f"Saved intermediate dataset at index {index} to: {dset_inter_name}")
                     
         return dset
 
@@ -171,27 +183,36 @@ class PosesGenerator(object):
     #     pass
         
     def run(self):
-        self.train_dataset = self.generate_bbs_and_pose(self.train_dataset)
-        self.val_dataset = self.generate_bbs_and_pose(self.val_dataset)
-        self.test_dataset = self.generate_bbs_and_pose(self.test_dataset)
+        # self.train_dataset = self.generate_bbs_and_pose(self.train_dataset)
+        # self.val_dataset = self.generate_bbs_and_pose(self.val_dataset)
+        # self.test_dataset = self.generate_bbs_and_pose(self.test_dataset)
         
-        train_path_name = self.config['data']['train'][:-12].split('/')[-1] #remove .mscoco.json
-        val_path_name = self.config['data']['val'][:-12].split('/')[-1] #remove .mscoco.json
-        test_path_name = self.config['data']['test'][:-12].split('/')[-1] #remove .mscoco.json
+        self.dataset = self.generate_bbs_and_pose(self.dataset)
         
-        train_path_with_pose = f"{self.config['data']['save_root']}/{train_path_name}_with_dets_and_pose.mscoco.json"
-        val_path_with_pose = f"{self.config['data']['save_root']}/{val_path_name}_with_dets_and_pose.mscoco.json"
-        test_path_with_pose = f"{self.config['data']['save_root']}/{test_path_name}_with_dets_and_pose.mscoco.json"
+        # train_path_name = self.config['data']['train'][:-12].split('/')[-1] #remove .mscoco.json
+        # val_path_name = self.config['data']['val'][:-12].split('/')[-1] #remove .mscoco.json
+        # test_path_name = self.config['data']['test'][:-12].split('/')[-1] #remove .mscoco.json
+        
+        
+        
+        # train_path_with_pose = f"{self.config['data']['save_root']}/{train_path_name}_with_dets_and_pose.mscoco.json"
+        # val_path_with_pose = f"{self.config['data']['save_root']}/{val_path_name}_with_dets_and_pose.mscoco.json"
+        # test_path_with_pose = f"{self.config['data']['save_root']}/{test_path_name}_with_dets_and_pose.mscoco.json"
+        
+        dataset_path_with_pose = f"{self.config['data']['save_root']}/{self.dataset_path_name}_with_dets_and_pose.mscoco.json"
         # print(f"train_path: {train_path}")
         # print(f"train_path_with_pose: {train_path_with_pose}")
-        self.train_dataset.dump(train_path_with_pose, newlines=True)
-        print(f"Saved train dataset to: {train_path_with_pose}")
+        # self.train_dataset.dump(train_path_with_pose, newlines=True)
+        # print(f"Saved train dataset to: {train_path_with_pose}")
         
-        self.val_dataset.dump(val_path_with_pose, newlines=True)
-        print(f"Saved val dataset to: {val_path_with_pose}")
+        # self.val_dataset.dump(val_path_with_pose, newlines=True)
+        # print(f"Saved val dataset to: {val_path_with_pose}")
         
-        self.test_dataset.dump(test_path_with_pose, newlines=True)
-        print(f"Saved test dataset to: {test_path_with_pose}")
+        # self.test_dataset.dump(test_path_with_pose, newlines=True)
+        # print(f"Saved test dataset to: {test_path_with_pose}")
+        
+        self.dataset.dump(dataset_path_with_pose, newlines=True)
+        print(f"Saved test dataset to: {dataset_path_with_pose}")
         
 
 def main():
